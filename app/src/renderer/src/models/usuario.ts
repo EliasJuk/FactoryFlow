7df18f57e89f0d1a5536945eb4b4d8798baeebb8
eliasjuk <eliasp.juk@hotmail.com>
2026-06-27 13:53:48 -0300

    update usuario.ts

--- app/src/renderer/src/models/usuario.ts
+++ app/src/renderer/src/models/usuario.ts
@@ -1,8 +1,10 @@
+export type PerfilUsuario = "OPERADOR" | "QUALIDADE" | "ADMIN"
+
 export interface Usuario {
   id: number
   matricula: string
   nome: string
   senha: string
-  perfil: "OPERADOR" | "QUALIDADE" | "ADMIN"
+  perfil: PerfilUsuario
   ativo: boolean
 }

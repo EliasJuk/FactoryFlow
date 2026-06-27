6104ce126e977e0e1d9570e11abd2a406dc8c38c
eliasjuk <eliasp.juk@hotmail.com>
2026-06-27 10:46:10 -0300

    Tela Login

--- app/src/renderer/src/main.tsx
+++ app/src/renderer/src/main.tsx
@@ -1,7 +1,9 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
-
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

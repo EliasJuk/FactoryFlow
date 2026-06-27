63d098e2901b2d9c435559bd688c009871e2e55f
eliasjuk <eliasp.juk@hotmail.com>
2026-06-27 12:41:47 -0300

    removido icon

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -1,21 +1,20 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import { useApp } from "../../contexts/AppContext"
 
 function createWindow(): void {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+const mainWindow = new BrowserWindow({
+  width: 900,
+  height: 670,
+  show: false,
+  autoHideMenuBar: true,
+  webPreferences: {
+    preload: join(__dirname, '../preload/index.js'),
+    sandbox: false
+  }
+})
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()

29b9bb34f73c0b3f7b0c0e9260cb98ac8466a5a1
eliasjuk <eliasp.juk@hotmail.com>
2026-06-30 18:28:05 -0300

    refactor(database): add import

--- app/src/main/database/connection.ts
+++ app/src/main/database/connection.ts
@@ -1,11 +1,12 @@
-import sqliteDb from "./database"
 import type { DatabaseConfig } from "./database.types"
+import Database from "better-sqlite3"
+import sqliteDb from "./database"
 
 const config: DatabaseConfig = {
   driver: "sqlite"
 }
 
-export function getDatabase() {
+export function getDatabase(): Database.Database  {
   if (config.driver === "sqlite") {
     return sqliteDb
   }

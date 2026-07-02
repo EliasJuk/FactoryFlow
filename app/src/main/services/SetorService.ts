4218597f190737d2b0c710b202b27b79abf401a1
eliasjuk <eliasp.juk@hotmail.com>
2026-07-01 21:39:54 -0300

    refactor(servicos): move SetorService para o processo principal

---
+++ app/src/main/services/SetorService.ts
@@ -0,0 +1,25 @@
+import { RepositoryProvider } from "../repositories/RepositoryProvider"
+
+export class SetorService {
+  private repository = RepositoryProvider.setores
+
+  async listar() {
+    return await this.repository.listar()
+  }
+
+  async criar(nome: string, sigla: string) {
+    return await this.repository.criar(nome, sigla)
+  }
+
+  async editar(id: number, nome: string, sigla: string) {
+    return await this.repository.editar(id, nome, sigla)
+  }
+
+  async excluir(id: number) {
+    return await this.repository.excluir(id)
+  }
+
+  async contarSubsetoresAtivos(id: number) {
+    return await this.repository.contarSubsetoresAtivos(id)
+  }
+}

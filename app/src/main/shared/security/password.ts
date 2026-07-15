df53f2c69153e17a5ea250040ee1347101d45549
eliasjuk <eliasp.juk@hotmail.com>
2026-07-15 18:26:30 -0300

    refactor(auth): simplify temporary password generation

--- app/src/main/shared/security/password.ts
+++ app/src/main/shared/security/password.ts
@@ -6,9 +6,7 @@ const ALGORITMO = 'sha512'
 
 export function gerarHashSenha(senha: string): string {
   const salt = crypto.randomBytes(16).toString('hex')
-  const hash = crypto
-    .pbkdf2Sync(senha, salt, ITERACOES, TAMANHO_CHAVE, ALGORITMO)
-    .toString('hex')
+  const hash = crypto.pbkdf2Sync(senha, salt, ITERACOES, TAMANHO_CHAVE, ALGORITMO).toString('hex')
 
   return `${salt}:${hash}`
 }
@@ -31,29 +29,6 @@ export function verificarSenha(senha: string, senhaHash: string): boolean {
 }
 
 export function gerarSenhaTemporaria(): string {
-  const letras = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-  const numeros = '23456789'
-  const simbolos = '@#_-'
-  const todos = `${letras}${numeros}${simbolos}`
-
-  const escolher = (conjunto: string) =>
-    conjunto[crypto.randomInt(0, conjunto.length)]
-
-  const caracteres = [
-    escolher('ABCDEFGHJKLMNPQRSTUVWXYZ'),
-    escolher('abcdefghijkmnopqrstuvwxyz'),
-    escolher(numeros),
-    escolher(simbolos)
-  ]
-
-  while (caracteres.length < 10) {
-    caracteres.push(escolher(todos))
-  }
-
-  for (let i = caracteres.length - 1; i > 0; i -= 1) {
-    const j = crypto.randomInt(0, i + 1)
-    ;[caracteres[i], caracteres[j]] = [caracteres[j], caracteres[i]]
-  }
-
-  return caracteres.join('')
+  const valor = crypto.randomInt(0, 10000)
+  return valor.toString().padStart(4, '0')
 }

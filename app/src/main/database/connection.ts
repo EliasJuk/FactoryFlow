36ea7543a03483bdef26015fe5330d9aaf1b47bc
eliasjuk <eliasp.juk@hotmail.com>
2026-07-22 18:43:29 -0300

    chore(postgres): remove connection test debug log

--- app/src/main/database/connection.ts
+++ app/src/main/database/connection.ts
@@ -5,11 +5,3 @@ type SqliteDatabase = typeof sqlite
 export function getDatabase(): SqliteDatabase {
   return sqlite
 }
-
-/*
-import type { Database as DatabaseType } from "better-sqlite3"
-import sqlite from "./sqlite/connection"
-
-export function getDatabase(): DatabaseType {
-  return sqlite
-}*/
